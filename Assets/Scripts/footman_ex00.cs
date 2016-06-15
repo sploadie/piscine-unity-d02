654ec833f1562df6f92588bb9a7707fe5bc6d1bb
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-15 17:26:05 +0200

    ex00 done

---
+++ Assets/Scripts/footman_ex00.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footman_ex00 : MonoBehaviour {
+
+	public Vector3 target;
+	public float direction;
+	public Vector3 direction_vector;
+	public float speed = 3.0f;
+	public float max_distance = 0.0f;
+
+	private Animator animator;
+	private float footman_z;
+	private bool targeting;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		footman_z = this.transform.position.z;
+		targeting = false;
+		animator.speed = 0;
+		animator.SetFloat ("Direction", -1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos;
+		if (Input.GetMouseButtonDown (0)) {
+			pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			pos.z = footman_z;
+			target = pos;
+			direction_vector = (pos - this.transform.position).normalized;
+			direction = Vector3.Dot (Vector3.up, direction_vector);
+			animator.SetFloat ("Direction", direction);
+			if (pos.x < this.transform.position.x) {
+				this.transform.localScale = new Vector3 (-1, 1, 1);
+			} else {
+				this.transform.localScale = new Vector3 (1, 1, 1);
+			}
+			targeting = true;
+			animator.speed = 1;
+		}
+		if (targeting) {
+			pos = this.transform.position;
+			float old_distance = (this.transform.position - target).magnitude;
+			Vector3 displacement = direction_vector * speed * Time.deltaTime;
+			Debug.Log ("Distance: "+old_distance+" Displacement: "+displacement.magnitude);
+			if (old_distance > displacement.magnitude) {
+				this.transform.position += displacement;
+			}
+			if (old_distance <= displacement.magnitude) {
+				this.transform.position = target - (direction_vector * (max_distance / 2));
+			}
+			if (old_distance <= displacement.magnitude + max_distance) {
+				targeting = false;
+				animator.speed = 0;
+				animator.Play("Walking",0, 0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/speed_test.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speed_test : MonoBehaviour {
+	
+	readonly int iterations = 10000000;
+	Vector3 testVector = new Vector3(3f, 14f, 42f);
+	
+	void Start()
+	{
+		Debug.Log(string.Format("TestConvertByOperation {0} instances: {1}s", iterations, TestConvertByOperation()));
+		Debug.Log(string.Format("TestConvertByCasting {0} instances: {1}s", iterations, TestConvertByCasting()));
+		Debug.Log(string.Format("TestConvertByInitializing {0} instances: {1}s", iterations, TestConvertByInitializing()));
+	}
+	
+	float TestConvertByOperation()
+	{
+		var timeStart = Time.realtimeSinceStartup;
+		
+		for (int i = 0; i < iterations; i++)
+		{
+			Vector2 v2 = testVector;
+		}
+		
+		return Time.realtimeSinceStartup - timeStart;
+	}
+	
+	float TestConvertByCasting()
+	{
+		var timeStart = Time.realtimeSinceStartup;
+		
+		for (int i = 0; i < iterations; i++)
+		{
+			Vector2 v2 = (Vector2)testVector;
+		}
+		
+		return Time.realtimeSinceStartup - timeStart;
+	}
+	
+	float TestConvertByInitializing()
+	{
+		var timeStart = Time.realtimeSinceStartup;
+		
+		for (int i = 0; i < iterations; i++)
+		{
+			Vector2 v2 = new Vector2(testVector.x, testVector.y);
+		}
+		
+		return Time.realtimeSinceStartup - timeStart;
+	}
+	
+}
